e81cc1056bba7f0d9a1f549c717a54b246cb4a2e
UniLory1908 <l.castellano4@studenti.unisa.it>
2024-01-25 03:51:38 +0100

    Valutazione città immessa dall'utente

---
+++ Modulo Machine Learning/InserimentoDati.py
@@ -0,0 +1,38 @@
+from EstrazioneDatiUsable import *
+from ModelloML import *
+
+
+# Funzione per ottenere i dati della nuova città dall'utente
+def ottieni_dati_citta():
+    citta = (input("Inserisci il nome del comune che ti interessa: "))
+
+    if not find_id_comune_by_name(citta):
+        print("Città non trovata, riprova")
+        ottieni_dati_citta()
+
+    latitudine, longitudine = trova_coordinate(citta)
+    pericolosita = calc_pericolosita(citta)
+    costo_vit = calc_spesa_media(trova_regione_da_comune(citta))
+    abitanti = trova_popolazione(citta)
+    superfice = trova_sup(citta)
+    abitanti_per_km2 = abitanti / superfice
+    num_negozi_km2 = trova_numero_poi_herev7(citta, Poi_Type.NEGOZIO.value) / superfice
+    num_ristoranti_km2 = trova_numero_poi_herev7(citta, Poi_Type.RISTORANTE.value) / superfice
+    num_scuole_km2 = trova_numero_poi_herev7(citta, Poi_Type.SCUOLA.value) / superfice
+
+    # Creare un DataFrame con i dati inseriti dall'utente
+    nuova_citta = pd.DataFrame({
+        'Latitudine': [latitudine],
+        'Longitudine': [longitudine],
+        'Pericolosità': [pericolosita],
+        'Costo Vita': [costo_vit],
+        'Abitanti': [abitanti],
+        'Abitanti per Km2': [abitanti_per_km2],
+        'Num Negozi Km2': [num_negozi_km2],
+        'Num Ristoranti Km2': [num_ristoranti_km2],
+        'Num Scuole Km2': [num_scuole_km2]
+    })
+
+    df_citta = pd.DataFrame(nuova_citta)
+    qualita_nuova_citta = model.predict(df_citta)
+    print("Qualità stimata: ", qualita_nuova_citta)
