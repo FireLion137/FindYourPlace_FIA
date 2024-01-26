43d36531c1caef77a8a1530702071f59d7db9daa
UniLory1908 <l.castellano4@studenti.unisa.it>
2024-01-26 05:04:21 +0100

    Fix errore ordine coordinate

--- Modulo Machine Learning/Disegna_box.py
+++ Modulo Machine Learning/Disegna_box.py
@@ -5,15 +5,15 @@ def draw_bbox_on_map(bbox):
 
     if bbox:
         # Calcola il centro del bounding box
-        center_lat = (bbox[0] + bbox[1]) / 2
-        center_lon = (bbox[2] + bbox[3]) / 2
+        center_lat = (bbox[1] + bbox[3]) / 2
+        center_lon = (bbox[0] + bbox[2]) / 2
 
         # Crea la mappa centrata sul bounding box
         map_object = folium.Map(location=[center_lat, center_lon], zoom_start=10)
 
         # Aggiungi il bounding box come rettangolo alla mappa
         folium.Rectangle(
-            bounds=[[bbox[0], bbox[2]], [bbox[1], bbox[3]]],
+            bounds=[[bbox[3], bbox[2]], [bbox[1], bbox[0]]],
             color='blue',
             fill=True,
             fill_color='blue',
@@ -21,10 +21,12 @@ def draw_bbox_on_map(bbox):
         ).add_to(map_object)
 
         # Salva la mappa su un file HTML
-        map_object.save('map_with_bbox.html')
+        map_filename = f'map_with_bbox.html'
+        map_object.save(map_filename)
+        print(f"Bounding box salvato in: {map_filename}")
         return True
     else:
-        print("Bounding box non disponibile.")
+        print(f"Bounding box non disponibile.")
         return False
 
 
@@ -33,3 +35,5 @@ def draw_bbox_on_map(bbox):
 bbox_example = (40.6115, 40.6259, 14.3611, 14.3886)
 draw_bbox_on_map(bbox_example)
 """
+
+# draw_bbox_on_map("Sorrento", "here")
